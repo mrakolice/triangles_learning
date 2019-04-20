aa623b3f17412a9d41762baf745efb1c7a4a02aa
mrakolice <postevoy.svyatoslav@gmail.com>
2019-04-20 17:42:39 +1000

    add error codes and screen create result

--- sources/entities.h
+++ sources/entities.h
@@ -1,24 +1,35 @@
 #ifndef ENTITIES_H
 #define ENTITIES_H
 	
-	typedef struct screen {
+	typedef struct {
 		int width;
 		int height;
 	} Screen;
 
-	typedef struct point {
+	typedef struct {
 		double x;
 		double y;
 	} Point;
 
-	typedef struct edge {
+	typedef struct {
 		Point start_point;
 		Point end_point;
 	} Edge;
 
-	typedef struct triangle {
-		Edge[3] edges;
-		Point[3] points;
+	typedef struct {
+		Edge edges[3];
+		Point points[3];
 	} Triangle;
 
+
+	typedef struct {
+		Screen result;
+		int error_code;
+	} ScreenCreateResult;
+
+
+	enum screen_error_codes {
+		ERROR_CODE_SUCCESS=0,
+		ERROR_CODE_NEGATIVE_HEIGHT=1
+	};
 #endif
