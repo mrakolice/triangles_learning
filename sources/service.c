b25b3554140cee320af6002df094be2137f7ca35
mrakolice <postevoy.svyatoslav@gmail.com>
2019-04-20 17:43:16 +1000

    create simple function create screen

---
+++ sources/service.c
@@ -0,0 +1,15 @@
+
+#include "service.h"
+
+ScreenCreateResult create_screen(int width, int height){
+	ScreenCreateResult screen_create_result;
+	
+	screen_create_result.result.width = width;
+	screen_create_result.result.height = height;
+
+	if (height < 0){
+		screen_create_result.error_code = ERROR_CODE_NEGATIVE_HEIGHT;
+	}
+
+	return screen_create_result;
+}
