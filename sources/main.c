21ac04148499936ef18c18cfd87da217016c1ed3
mrakolice <postevoy.svyatoslav@gmail.com>
2019-04-27 14:19:01 +1000

    create high level api

--- sources/main.c
+++ sources/main.c
@@ -2,8 +2,24 @@
 #include "main.h"
 
 int main(){
-	printf("Я родился\n");
 	run_tests();
+
+	int width, height, iteration_count;
+	double curve_coefficient;
+
+	width = input_int("Введите ширину экрана");
+	height = input_int("Введите высоту экрана");
+	iteration_count = input_int("Введите количество итераций");
+	curve_coefficient = input_double("Введите коэффициент кривизны");
+
+	ScreenCreateResult screen_create_result = create_screen(width, height);
+	if (screen_create_result.error_code){
+		output_screen_create_result_error_code(screen_create_result.error_code);
+		return 0;
+	}
+
+	Triangle* triangles = calculate_all(screen_create_result.result, iteration_count, curve_coefficient);
+	output_triangles(triangles);
 
 	return 0;
 }
--- sources/main.h
+++ sources/main.h
@@ -3,6 +3,8 @@
 	#include "tests.h"
 	
 	#include "service.h"
+	#include "input.h"
+	#include "output.h"
 	
 
 #endif
